731ad46be1a9888be460fdd9d81e5f040b887bc7
Dominik Němeček <dnemecek52@gmail.com>
2025-10-04 19:09:51 +0200

    Aktualizován kód v SP4

--- 00-main/ulohy-z-hodiny/03-hodina-zaklady-csharp/sp4-vekova-kategorie/sp4-vekova-kategorie/Program.cs
+++ 00-main/ulohy-z-hodiny/03-hodina-zaklady-csharp/sp4-vekova-kategorie/sp4-vekova-kategorie/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // === Věková kategorie - samostatná práce č.4 ===
+
+        // --- Zadání věku od uživatele ---
+        Console.Write("Zadejte svůj věk: ");
+        int age;
+
+        // Ošetření vstupu - cyklus se opakuje, dokud uživatel nezadá kladné celé číslo
+        while (!int.TryParse(Console.ReadLine(), out age) || age < 0)
+        {
+            Console.WriteLine("Neplatný vstup. Zadejte prosím kladné číslo pro věk:");
+        }
+
+        // --- Určení kategorie pomocí switch expression ---
+        string category = age switch
+        {
+            <= 12 => "dítě", // pokud je věk 12 nebo méně -> vrátí dítě
+            <= 19 => "teenager", // pokud je věk 19 nebo méně -> vrátí teenager
+            <= 64 => "dospělý", // pokud je věk 64 nebo méně -> vrátí dospělý
+            _ => "senior" // pro všechny ostatní případy (65 a více) -> vrátí senior
+        };
+
+        // --- Výstup výsledku ---
+        Console.WriteLine($"Jste {category}.");
+    }
+}
