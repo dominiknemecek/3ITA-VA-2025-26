7c92cc3e849e67250371a58ac5ad5266b40958f2
Dominik Němeček <dnemecek52@gmail.com>
2025-10-04 18:50:05 +0200

    Aktualizován kód v SP6 - Seřazení čísel

--- 00-main/ulohy-z-hodiny/03-hodina-zaklady-csharp/sp6-serazeni-cisel/sp6-serazeni-cisel/Program.cs
+++ 00-main/ulohy-z-hodiny/03-hodina-zaklady-csharp/sp6-serazeni-cisel/sp6-serazeni-cisel/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // --- Načtení prvního čísla ---
+        Console.Write("Zadejte první číslo: ");
+        int x;
+        // Ošetření neplatného vstupu – opakuj, dokud uživatel nezadá celé číslo
+        while (!int.TryParse(Console.ReadLine(), out x))
+        {
+            Console.WriteLine("Neplatná volba. Zadejte číslo znovu!");
+        }
+
+        // --- Načtení druhého čísla ---
+        Console.Write("Zadejte druhé číslo: ");
+        int y;
+        // Opět kontrola vstupu pomocí TryParse
+        while (!int.TryParse(Console.ReadLine(), out y))
+        {
+            Console.WriteLine("Neplatná volba. Zadejte číslo znovu!");
+        }
+
+        // --- Načtení třetího čísla ---
+        Console.Write("Zadejte třetí číslo: ");
+        int z;
+        while (!int.TryParse(Console.ReadLine(), out z))
+        {
+            Console.WriteLine("Neplatná volba. Zadejte číslo znovu!");
+        }
+
+        // --- Hledání nejmenšího, prostředního a největšího čísla ---
+        // Nejmenší číslo (min) – porovnání všech tří pomocí ternárních operátorů
+        int min = (x < y && x < z) ? x : (y < x && y < z ? y : z);
+
+        // Největší číslo (max) – obdobně porovnáme všechna tři čísla
+        int max = (x > y && x > z) ? x : (y > x && y > z ? y : z);
+
+        // Prostřední číslo (mid) zjistíme jednoduše:
+        // součet všech tří čísel minus nejmenší a největší
+        int mid = (x + y + z) - min - max;
+
+        // --- Výstup výsledků ---
+        Console.WriteLine($"Seřazená čísla: x = {min}, y = {mid}, z = {max}");
+    }
+}
